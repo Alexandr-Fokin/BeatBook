9f6f457d768c8ae88d1560f0422b69f723043985
Alexandr <fokin.alek@mail.ru>
2025-10-21 20:47:20 +0500

    build app

--- src/components/popup/Popup.jsx
+++
@@ -1,8 +0,0 @@
-export default function Popup(props) {
-  return (
-    <div className="popup__block">
-      <div className="popup__overlay"></div>
-      <div className="popup__block-inner">{props.children}</div>
-    </div>
-  );
-}
--- src/dataStore.js
+++
@@ -1,4 +0,0 @@
-export const DataStore = {
-    getUserData: () => JSON.parse(localStorage.getItem('userData') || '{"albums": [], "tracks": [], "folders": [{"id":"liked", "name": "Любимые альбомы"}, {"id":"test", "name": "Тестовая папка"}]}'),
-    saveUserData: (data) => localStorage.setItem('userData', JSON.stringify(data))
-} 
